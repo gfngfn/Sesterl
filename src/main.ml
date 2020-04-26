004f4853042bf220f12d444dfe8d8c160b99cf60
gfngfn <suwa-takashi141@g.ecc.u-tokyo.ac.jp>
2020-04-26 17:40:58 +0900

    slightly modify how to display errors

--- src/main.ml
+++ src/main.ml
@@ -21,7 +21,7 @@ let () =
       Format.printf "%a: syntax error\n" Range.pp rng
 
   | UnidentifiedToken(rng, s) ->
-      Format.printf "%a: unidentified token\n" Range.pp rng
+      Format.printf "%a: unidentified token '%s'\n" Range.pp rng s
 
   | SeeEndOfFileInComment(rng) ->
       Format.printf "%a: unclosed comment\n" Range.pp rng
@@ -38,7 +38,7 @@ let () =
 
   | Typechecker.InclusionError(fid, ty1, ty2) ->
       let (rng1, _) = ty1 in
-      Format.printf "%a: this expression has type %a and type %a at the same time, but is inconsistent as to the occurrence of type variable %a\n"
+      Format.printf "%a: this expression has type %a and type %a at the same time, but these types are inconsistent as to the occurrence of type variable %a\n"
         Range.pp rng1
         pp_mono_type ty1
         pp_mono_type ty2
