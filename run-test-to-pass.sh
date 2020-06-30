a63a921e32a9226202f9f8e07d86bde72264967e
gfngfn <suwa-takashi141@g.ecc.u-tokyo.ac.jp>
2020-06-30 23:26:18 +0900

    improve printing by 'run-test-to-pass.sh'

--- run-test-to-pass.sh
+++ run-test-to-pass.sh
@@ -4,12 +4,14 @@ BIN="./sesterl"
 TARGET_DIR="test/_generated/"
 
 compile_erlang_code() {
-  for TARGET in *.erl; do
-      erlc "$TARGET"
-  done
+    for TARGET in *.erl; do
+        echo "Compiling '$TARGET' by erlc ..."
+        erlc "$TARGET"
+    done
 }
 
 for SOURCE in test/pass/*.sest; do
+    echo "Compiling '$SOURCE' by sesterl ..."
     "$BIN" "$SOURCE" -o "$TARGET_DIR"
 done
 
