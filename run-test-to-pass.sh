1334f05dbf22bf0cd8ed579e8ed15ae148946c17
gfngfn <suwa-takashi141@g.ecc.u-tokyo.ac.jp>
2020-07-04 01:01:47 +0900

    slightly refactor blackbox tests about 'erlc'

--- run-test-to-pass.sh
+++ run-test-to-pass.sh
@@ -1,18 +1,16 @@
 #!/bin/bash
 
 BIN="./sesterl"
-TARGET_DIR="test/_generated/"
+SOURCE_DIR="test/pass"
+TARGET_DIR="test/_generated"
 
-compile_erlang_code() {
-    for TARGET in *.erl; do
-        echo "Compiling '$TARGET' by erlc ..."
-        erlc "$TARGET"
-    done
-}
 
-for SOURCE in test/pass/*.sest; do
+for SOURCE in "$SOURCE_DIR"/*.sest; do
     echo "Compiling '$SOURCE' by sesterl ..."
     "$BIN" "$SOURCE" -o "$TARGET_DIR"
 done
 
-cd "$TARGET_DIR" && compile_erlang_code
+for TARGET in "$TARGET_DIR"/*.erl; do
+    echo "Compiling '$TARGET' by erlc ..."
+    erlc -o "$TARGET_DIR" "$TARGET"
+done
