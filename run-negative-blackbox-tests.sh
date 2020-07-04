2cb9022a2a02ffdb736ae2174bd9d3852fb1bf11
gfngfn <suwa-takashi141@g.ecc.u-tokyo.ac.jp>
2020-07-04 17:19:35 +0900

    develop negative blackbox tests

---
+++ run-negative-blackbox-tests.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+BIN="./sesterl"
+SOURCE_DIR="test/fail"
+TARGET_DIR="test/_generated"
+
+NO_ERRORS=()
+
+for SOURCE in "$SOURCE_DIR"/*.sest; do
+    echo "Compiling '$SOURCE' by sesterl ..."
+    "$BIN" "$SOURCE" -o "$TARGET_DIR"
+    STATUS=$?
+    if [ $STATUS -eq 0 ]; then
+        NO_ERRORS+=("$SOURCE")
+    fi
+done
+
+RET=0
+for X in "${NO_ERRORS[@]}"; do
+    RET=1
+    echo "[FAIL] $X"
+done
+if [ $RET -eq 0 ]; then
+    echo "All tests have passed."
+fi
+
+exit $RET
