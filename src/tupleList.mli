4a0ee379cc12d6b3897693798bbefb8603de6c92
gfngfn <suwa-takashi141@g.ecc.u-tokyo.ac.jp>
2020-09-14 06:16:19 +0900

    add doc comments to 'TupleList'

--- src/tupleList.mli
+++ src/tupleList.mli
@@ -1,10 +1,13 @@
 
 type 'a t
+(** ['a t] is the type for lists (of values of type ['a]) the length of which is more than or equal to 2. *)
 
 val make : 'a -> 'a -> 'a list -> 'a t
+(** [make e1 e2 es] corresponds to [e1 :: e2 :: es]. *)
 
 val map : ('a -> 'b) -> 'a t -> 'b t
 
 val to_list : 'a t -> 'a list
+(** [to_list] forgets the constraint of the length. *)
 
 val pp : (Format.formatter -> 'a -> unit) -> Format.formatter -> 'a t -> unit
