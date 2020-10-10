3f96ae445badaa596790bb0ee1763f090be05c14
gfngfn <suwa-takashi141@g.ecc.u-tokyo.ac.jp>
2020-10-10 22:48:07 +0900

    require config files to have 'dependencies' field

--- src/configLoader.ml
+++ src/configLoader.ml
@@ -2,11 +2,43 @@
 open MyUtil
 
 
+type dependency_source =
+  | Local of absolute_path
+
+type dependency = {
+  dependency_name   : string;
+  dependency_source : dependency_source;
+}
+
 type config = {
   package_name       : string;
   main_module_path   : absolute_path;
   source_directories : absolute_dir list;
+  dependencies       : dependency list;
 }
+
+
+let source_decoder (confdir : absolute_dir) : dependency_source YamlDecoder.decoder =
+  let open YamlDecoder in
+  let ( >>= ) = bind in
+  get "type" string >>= function
+  | "local" ->
+      get "directory" string >>= fun dirstr ->
+      succeed (Local(make_absolute_path confdir dirstr))
+
+  | other ->
+      failure (Printf.sprintf "unsupported type '%s' for specifying dependency sources" other)
+
+
+let dependency_decoder (confdir : absolute_dir) : dependency YamlDecoder.decoder =
+  let open YamlDecoder in
+  let ( >>= ) = bind in
+  get "name" string >>= fun name ->
+  get "source" (source_decoder confdir) >>= fun source ->
+  succeed {
+    dependency_name   = name;
+    dependency_source = source;
+  }
 
 
 let config_decoder (confdir : absolute_dir) : config YamlDecoder.decoder =
@@ -15,11 +47,13 @@ let config_decoder (confdir : absolute_dir) : config YamlDecoder.decoder =
   get "package" string >>= fun package_name ->
   get "source_directories" (list string) >>= fun srcdirs ->
   get "main_module" string >>= fun mainpath ->
+  get_or_else "dependencies" (list (dependency_decoder confdir)) [] >>= fun dependencies ->
   let config =
     {
       package_name       = package_name;
       main_module_path   = make_absolute_path confdir mainpath;
       source_directories = List.map (make_absolute_path confdir) srcdirs;
+      dependencies       = dependencies;
     }
   in
 (*
