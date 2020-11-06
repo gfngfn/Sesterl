a361d85c22bcd4eddfe54cacadde6fad54ba49d9
gfngfn <suwa-takashi141@g.ecc.u-tokyo.ac.jp>
2020-11-07 05:14:19 +0900

    make the compiler perform 'mkdir -p <output-dir>'

--- src/main.ml
+++ src/main.ml
@@ -43,6 +43,7 @@ let main (fpath_in : string) (dir_out : string) (is_verbose : bool) =
     in
 
     (* Generate and output code corresponding to each package. *)
+    Core.Unix.mkdir_p dir_out;
     let (_, gmap) = Primitives.initial_environment in
     pkgoutsacc |> Alist.to_list |> List.fold_left (fun gmap (pkgnameopt, outs) ->
       outs |> List.fold_left (fun gmap (sname, binds) ->
