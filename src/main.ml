4edffc79e8662283c26a0e7e04aa4a900f6c684d
gfngfn <suwa-takashi141@g.ecc.u-tokyo.ac.jp>
2021-05-16 10:28:47 +0900

    upgrade the version

--- src/main.ml
+++ src/main.ml
@@ -175,7 +175,7 @@ let command_main =
     Term.(ret (const (`Error(true, "No subcommand specified."))))
   in
   let info : Term.info =
-    Term.info ~version:"0.1.0" "sesterl"
+    Term.info ~version:"0.1.1" "sesterl"
   in
   (term, info)
 
